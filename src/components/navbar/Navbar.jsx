bfe0fe1b544eb824143bf3e9703949e1dea6aaeb
hardcorekid03 <parasapremiumko88@gmail.com>
2024-05-10 17:28:47 +0800

    toml

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,6 +9,7 @@ import logo from "/src/assets/logo.svg";
 import { Link } from "react-router-dom";
 
 function OffcanvasExample() {
+  
   const [scrolled, setScrolled] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -59,7 +60,7 @@ function OffcanvasExample() {
               <Offcanvas.Body>
                 <Nav className="justify-content-center flex-grow-1 pe-3">
                   <Nav.Link
-                  collapseOnSelect
+                  
                     as={Link}
                     to="/"
                     className="nav-link link poppins-medium"
